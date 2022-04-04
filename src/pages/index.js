4c32241da2ae23fdecfd5866cd44000d67a966e8
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2022-04-05 02:55:38 +0530

    renameing file 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,4 +5,4 @@ export { MyPlaylist } from "./MyPlaylist";
 export { WatchLater } from "./WatchLater";
 export { LikedVids } from "./LikedVids";
 export { Settings } from "./Settings";
-export { SingleVideo } from "./SingleVideo";
+export { SingleVideo } from "./SiingleVideo";
